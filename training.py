28bc738b22a70b07a74e1755828b706195c010b0
Steven Abreu <stevenabreu7@gmail.com>
2018-10-15 21:54:03 -0400

    minor fixes for training

--- training.py
+++ training.py
@@ -124,36 +124,34 @@ class Trainer:
                 batch_data = batch_data.cuda() if self.gpu else batch_data
                 batch_labels = batch_labels.cuda() if self.gpu else batch_labels
 
-                # forward pass of the data
+                # forward pass of the data and compute loss
                 batch_output = self.net(batch_data)
-
-                # evaluate the prediction and correctness
-                batch_prediction = batch_output.data.max(1, keepdim = True)[1]
-                batch_prediction = batch_prediction.eq(batch_labels.data.view_as(batch_prediction))
-                train_correct += batch_prediction.sum()
-                train_num += batch_data.data.shape[0]
-
-                # compute the losss
                 batch_loss = self.criterion(batch_output, batch_labels)
                 
-                # backward pass and optimizer step
+                # evaluate the prediction
+                batch_prediction = torch.max(batch_output, 1)[1]
+
+                # backward pass
                 batch_loss.backward()
                 self.optimizer.step()
 
-                # sum up this batch's loss
+                # train stats
+                train_correct += (batch_prediction == batch_labels).sum().cpu().item()
+                train_num += batch_data.data.shape[0]
                 train_loss += batch_loss.data.item()
 
                 # print training progress
                 if batch_i % 10 == 0:
-                    print('\rEpoch {:3} Progress {:7.2%} Accuracy {:7.2%}'.format(
+                    print('\rEpoch {:3} Progress {:7.2%} Accuracy {:7.2%} Loss {:7.4f}'.format(
                         epoch + 1, 
                         batch_i * self.train_loader.batch_size / len(self.train_loader.dataset),
-                        train_correct.cpu().item() / ((batch_i + 1) * self.train_loader.batch_size)
+                        train_correct / train_num,
+                        train_loss / (batch_i + 1)
                     ), end='')
 
             # compute epoch loss and accuracy
-            train_loss = train_loss / train_num
-            train_accuracy = train_correct.cpu().item() / train_num
+            train_loss = train_loss / len(self.train_loader)
+            train_accuracy = train_correct / train_num
 
             # print summary for this epoch
             print('\rEpoch {:3} finished.\t\t\t\nTraining Accuracy: {:5.2%}\nTraining Loss: {:10.7f}'.format(
@@ -161,63 +159,6 @@ class Trainer:
                 train_accuracy, 
                 train_loss
             ))
-
-            """ Ignore validation data for now
-
-            ##############################
-            # VALIDATION DATA
-            ##############################
-
-            val_num = 0
-            val_loss = 0
-            val_correct = 0
-
-            for batch_i, (batch_datas, batch_labels) in enumerate(self.val_loader):
-
-                batch_data1, batch_data2 = batch_datas
-
-                # initialize the data as variables
-                batch_data1 = Variable(batch_data1.view(-1, self.train_loader.dataset.el_length))
-                # move data to GPU if possible
-                batch_data1 = batch_data1.cuda() if self.gpu else batch_data1
-                # forward pass of the data
-                batch_output1 = self.net(batch_data1)
-
-                # initialize the data as variables
-                batch_data2 = Variable(batch_data2.view(-1, self.train_loader.dataset.el_length))
-                # move data to GPU if possible
-                batch_data2 = batch_data2.cuda() if self.gpu else batch_data2
-                # forward pass of the data
-                batch_output2 = self.net(batch_data2)
-
-                # labels
-                batch_labels = Variable(batch_labels)
-                batch_labels = batch_labels.cuda() if self.gpu else batch_labels
-
-
-
-                # evaluate the prediction and correctness
-                batch_prediction = batch_output.data.max(1, keepdim = True)[1]
-                batch_prediction = batch_prediction.eq(batch_labels.data.view_as(batch_prediction))
-                val_correct += batch_prediction.sum()
-                val_num += batch_data.data.shape[0]
-
-                # compute the losss
-                batch_loss = self.criterion(batch_output, batch_labels)
-
-                # sum up this batch's loss
-                val_loss += batch_loss.data.item()
-
-            # compute validation loss and accuracy
-            val_loss = val_loss / val_num
-            val_accuracy = val_correct.cpu().item() / val_num
-
-            # print validation stats
-            print('Validation Accuracy: {:5.2%}\nValidation Loss: {:10.7f}'.format(
-                val_accuracy, 
-                val_loss
-            ))
-            """
 
             torch.save(self.net, 'models/{}_{}'.format(self.name, epoch))
 
@@ -241,7 +182,9 @@ def main():
     net = models.Resnet(train_dataset._nspeak, alpha=16, frames=max_length)
     
     # initialization
-    # net.apply(nn.init.kaiming_normal_)
+    for layer in net:
+        if isinstance(layer, nn.Conv2d):
+            nn.init.kaiming_normal_(layer.weight)
 
     # training parameters
     optimizer = torch.optim.SGD(net.parameters(), nesterov=True, momentum=0.01, dampening=0, lr=0.01, weight_decay=0.01)
@@ -249,7 +192,8 @@ def main():
     criterion = nn.modules.loss.CrossEntropyLoss()
 
     # initialize trainer
-    trainer = Trainer(train_loader, val_loader, 'resnet', net, optimizer, criterion, scheduler)
+    name = 'resnet'
+    trainer = Trainer(train_loader, val_loader, name, net, optimizer, criterion, scheduler)
 
     # run the training
     trainer.train(epochs)
